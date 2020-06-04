ad46b9bc59baae1d344b9bd3515e7eb4000c1825
Mashkovska <32987039+Mashkovska@users.noreply.github.com>
2020-06-04 19:37:01 +0300

    first version

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import {
+  StyleSheet,
+  View,
+  AppRegistry,
+  Button,
+  Text,
+  FlatList,
+} from "react-native";
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dataSource: [],
+    };
+  }
+
+  renderItem = () => {
+    <View>
+      <Text>{item.name}</Text>
+      <Text>{item.name}</Text>
+      <Text>{item.name}</Text>
+    </View>;
+  };
+
+  componentDidMount() {
+    const url =
+      "http://www.json-generator.com/api/json/get/cfBkjqdzuG?indent=2";
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          dataSource: responseJson.teachers,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList data={this.state.dataSource} renderItem={this.renderItem} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
